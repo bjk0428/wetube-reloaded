ed148629ee37025f5928b5687ae3bfb68c6d15ed
Jokyoung <bjk0428@naver.com>
2022-05-12 05:34:22 +0900

    unit6

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter"; 
+import { trending } from "./Controllers/videoContoller";
 
 const PORT = 4000;
 
@@ -12,6 +13,8 @@ app.use(logger);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended : true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
