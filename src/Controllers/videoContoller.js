1dc413067848366535089208cadad1fb39858a39
Jokyoung <bjk0428@naver.com>
2022-05-17 22:32:54 +0900

    async

--- src/Controllers/videoContoller.js
+++ src/Controllers/videoContoller.js
@@ -1,17 +1,16 @@
+import { defaultConfiguration } from "express/lib/application";
 import Video from "../models/Video";
 
 const handleSearch = (error, videos) => {
     console.log("errors", error);
     console.log("videos", videos);
 }
+// Video.find({}, (error, videos) => {});
 
-export const home = (req, res) => {
-    Video.find({}, (error, videos) => {    
-    console.log("errors", error);
-    console.log("videos", videos);
-});
-console.log("hello");
-    return res.render("home", {pageTitle: "Home" , videos: [] } );
+export const home = async(req, res) => {
+    const videos = await Video.find({});
+    console.log(videos);
+    return res.render("home", {pageTitle: "Home" , videos } );
 };
 export const watch = (req, res) =>{
     const { id } = req.params;
@@ -31,8 +30,26 @@ export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: `Upload Video`});
 };
 
-export const postUpload = (req, res) => {
-    const {title} = req.body;
-    return res.redirect("/");
+export const postUpload = async(req, res) => {
+    const {title, description, hashtags } = req.body;
+    try{
+        await Video.create({
+            title,
+            description,
+            createdAt: Date.now(),
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            meta: {
+                views: sdfkjdlkfjslkfjsdfldsj,
+                rating: 0
+            }
+        });
+        return res.redirect("/");
+    }catch(error){
+        return res.render("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: error,
+        });
+    }
+
 }
 
